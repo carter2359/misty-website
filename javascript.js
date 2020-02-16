dbbd2871834330789f2379bee0aefc512db0c651
Samantha1350 <57157589+Samantha1350@users.noreply.github.com>
2020-02-15 23:37:14 -0800

    first commit of all associated documents

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/* 
+    Date: February 15, 2020
+    Filename: javascript.js
+    Language: JavaScript
+    Editor: Visual Studio Code
+    Operating System Used: Windows 10 
+*/
+
+/* show side panel */
+function openNav() {
+    document.getElementById("nav").style.width = "200px";
+  }
+  
+  /* hide side panel */
+  function closeNav() {
+    document.getElementById("nav").style.width = "0";
+  }
