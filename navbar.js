a6513d43b9b6b908ea1360d1519ace34bdb5c5ec
unknown <westpeak@shaw.ca>
2020-02-17 16:42:42 -0800

    completed all construction pages

---
+++ navbar.js
@@ -0,0 +1,17 @@
+/* 
+    Date: February 17, 2020
+    Filename: navbar.js
+    Language: JavaScript
+    Editor: Visual Studio Code
+    Operating System Used: Windows 10 
+*/
+
+/* show side panel */
+function openNav() {
+    document.getElementById("nav").style.width = "200px";
+  }
+  
+  /* hide side panel */
+  function closeNav() {
+    document.getElementById("nav").style.width = "0";
+  }
